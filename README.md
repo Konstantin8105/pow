120354668f54ff85c7bcd9113fd46e9933f903ba
Konstantin8105 <konstantin8105@gmail.com>
2018-12-30 21:47:31 +0300

    benchmark check

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # pow
 replace golang math.Pow to optimal
+
+*benchmark*
+
+```
+go test -bench=. -count=5 > bench.txt
+benchstat bench.txt 
+```
+```
+name          time/op
+/math.Pow2-4   357ns ± 0%
+/pow.E2-4     5.69ns ± 0%
+
+/math.Pow3-4   359ns ± 1%
+/pow.E3-4     8.86ns ± 1%
+```
+
+So, that package is more optimal at more 30 times.
---
+++ pow_test.go
@@ -0,0 +1,81 @@
+package pow_test
+
+import (
+	"fmt"
+	"math"
+	"testing"
+
+	"github.com/Konstantin8105/pow"
+)
+
+func TestE2(t *testing.T) {
+	tcs := []struct {
+		x      float64
+		result float64
+	}{
+		{1.0, 1.0},
+		{2.0, 4.0},
+		{3.0, 9.0},
+		{math.NaN(), math.NaN()},
+		{math.Inf(1), math.Inf(1)},
+		{math.Inf(-1), math.Inf(1)},
+	}
+	for i := range tcs {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			a := pow.E2(tcs[i].x)
+			e := math.Pow(tcs[i].x, 2.0)
+			if a != e && math.IsNaN(a) != math.IsNaN(e) {
+				t.Errorf("got %14e , want %.14e", a, e)
+			}
+		})
+	}
+}
+
+func TestE3(t *testing.T) {
+	tcs := []struct {
+		x      float64
+		result float64
+	}{
+		{1.0, 1.0},
+		{2.0, 8.0},
+		{3.0, 27.0},
+		{math.NaN(), math.NaN()},
+		{math.Inf(1), math.Inf(1)},
+		{math.Inf(-1), math.Inf(-1)},
+	}
+	for i := range tcs {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			a := pow.E3(tcs[i].x)
+			e := math.Pow(tcs[i].x, 3.0)
+			if a != e && math.IsNaN(a) != math.IsNaN(e) {
+				t.Errorf("got %14e , want %.14e", a, e)
+			}
+		})
+	}
+}
+
+func Benchmark(b *testing.B) {
+	x := math.Pi
+	var y float64
+	b.Run("math.Pow2", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			y = math.Pow(x, 2.0)
+		}
+	})
+	b.Run("pow.E2", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			y = pow.E2(x)
+		}
+	})
+	b.Run("math.Pow3", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			y = math.Pow(x, 3.0)
+		}
+	})
+	b.Run("pow.E3", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			y = pow.E3(x)
+		}
+	})
+	_ = y
+}
